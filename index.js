565fd8877847923662bc4993364da77a849f9ab4
Adam Fisher <ae.fisher.gains@gmail.com>
2017-10-10 13:53:01 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,11 @@
+var katzDeliLine = [];
+//take a number function
+function takeANumber (katzDeliLine, name) {
+  katzDeliLine.push(name);
+  var deliLength = katzDeliLine.length
+  return (`Welcome, ${name}. You are number ${deliLength} in line.`);
+}
+//now serving function
+function nowServing (katzDeliLine) {
+  if katzDeliLine.length > 0 
+}
