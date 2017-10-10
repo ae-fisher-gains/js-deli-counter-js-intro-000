cecd9725bb1a5ec722c2a0f7b01926348111dc3d
Adam Fisher <ae.fisher.gains@gmail.com>
2017-10-10 14:02:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,13 @@ function takeANumber (katzDeliLine, name) {
   return (`Welcome, ${name}. You are number ${deliLength} in line.`);
 }
 //now serving function
-
+function nowServing (katzDeliLine) {
+  if katzDeliLine.length == 0 {
+    return "There is nobody waiting to be served!"
+  }
+  else {
+    var firstInLine = katzDeliLine.slice(1)
+    katzDeliLine.shift()
+    return `Currently serving ${firstInLine}.`
+  }
+}
